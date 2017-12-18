bcfd6babc634b2df797ca706cfc2a2920db5ccb1
Unknown <vuchuongmb@gmail.com>
2017-12-19 01:10:26 +0200

    book

--- main.js
+++ main.js
@@ -35,6 +35,18 @@
 
       });
   });
+
+  $(document).ready(function(){
+      $("#flip1").click(function(){
+          $("#panel1").slideToggle("slow");
+      });
+  });
+  $(document).ready(function(){
+      $("#flip2").click(function(){
+          $("#panel2").slideToggle("slow");
+      });
+  });
+
 })(jQuery);
 
 
